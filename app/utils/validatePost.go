fac764f6e464aa02d576ede9b412956863660c43
A-fethi <fethiabderrahmane1@gmail.com>
2025-02-04 16:39:53 +0100

    adding upload image implementation

---
+++ app/utils/validatePost.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"fmt"
+	"forum/app/config"
+)
+
+func ValidatePost(title, content, image string) error {
+	if title == "" {
+		config.Logger.Println("Validation failed: Title is empty")
+		return fmt.Errorf("title cannot be empty")
+	}
+	if content == "" {
+		config.Logger.Println("Validation failed: Content is empty")
+		return fmt.Errorf("content cannot be empty")
+	}
+	if image == "" {
+		config.Logger.Println("Validation failed: Image is empty")
+		return fmt.Errorf("image cannot be empty")
+	}
+
+	if len(title) > 100 {
+		config.Logger.Printf("Validation failed: Title length is invalid (title length: %d)", len(title))
+		return fmt.Errorf("title must be between 5 and 100 characters")
+	}
+
+	if len(content) > 5000 {
+		config.Logger.Printf("Validation failed: Content length is invalid (content length: %d)", len(content))
+		return fmt.Errorf("content must be between 10 and 5000 characters")
+	}
+
+	config.Logger.Println("Validation successful for title and content")
+	return nil
+}
